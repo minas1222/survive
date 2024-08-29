490438a44b7e4491333afb2a20922d2146991761
minas1222 <minas.times@gmail.com>
2024-08-29 19:15:15 +0900

    chore: construct side project

---
+++ src/main/java/com/minas/webside/WebsideApplication.java
@@ -0,0 +1,13 @@
+package com.minas.webside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minas/webside/WebsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minas.webside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
